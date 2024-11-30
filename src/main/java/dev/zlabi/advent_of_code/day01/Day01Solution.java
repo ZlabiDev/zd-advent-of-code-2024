62e20d0ca059f92aaa7042d27bd94fd084b560c7
ZlabiDev <zlabi.dev@gmail.com>
2024-11-30 02:04:45 +0100

    Day 01 scaffold added

---
+++ src/main/java/dev/zlabi/advent_of_code/day01/Day01Solution.java
@@ -0,0 +1,8 @@
+package dev.zlabi.advent_of_code.day01;
+
+public class Day01Solution {
+
+    public static void main(String[] args) {
+        // TODO add Day 01 Solution
+    }
+}
---
+++ src/main/java/dev/zlabi/advent_of_code/day01/README.md
@@ -0,0 +1,4 @@
+## Advent of Code - Day 01
+https://adventofcode.com/2024/day/1
+
+### Question
